8121659ad32e0648f05f867ac50ca5c706d5610a
Alfonso Franco <alfonso.frayala@gmail.com>
2024-07-26 17:05:11 -0600

    Se agregó contenido a la historia y opciones de software

--- Historia.py
+++ Historia.py
@@ -1,2 +1,5 @@
 print("Había una vez...")
 # Añade prints contando una historia (la historia debe tener sentido respecto al contexto anterior).
+
+print('unos estudiantes apasaionados por el futuro...')
+print('quienes para prepararse, empezaron una idea...')
