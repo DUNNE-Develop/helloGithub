34abc267bffd98094a51e5f33f9e245d4cf5d669
Daniel Isidoro Chaguen Hernandez <danielchagueno@gmail.com>
2024-07-26 17:12:11 -0600

    continuación historia y las 5 herramientas

--- Historia.py
+++ Historia.py
@@ -1,2 +1,4 @@
 print("Había una vez...")
 # Añade prints contando una historia (la historia debe tener sentido respecto al contexto anterior).
+print("un pulpo que nada libremente cerca del fondo marino.")
+print("Hasta que un día se preguntó, ¿qué habrá fuera del mar que lo rodea?")
